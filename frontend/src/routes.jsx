08e029775fbdc5e0c49fbeb02ca48a0fa6f827a3
Damaris200 <151961679+Damaris200@users.noreply.github.com>
2025-04-25 04:32:29 +0100

    update

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -25,6 +25,7 @@ import ForgotPassword from "./pages/ForgetPassword";
 import ProtectiveWrapper from "./components/ProtectiveWrapper";
 import SignInModal from "./components/SignInModal";
 import { useEffect, useState } from "react";
+import JobHomePage from "./pages/job.pages/JobHomePage";
 import About from "./pages/About";
 import ContactPage from "./pages/Contact";
 import StartupDetail from "@/pages/StartupDetial";
@@ -35,6 +36,9 @@ import Candidate from "@/pages/Candidates";
 import ContactCandidate from "@/pages/ContactCandidate";
 import ViewReport from "@/pages/ViewReport"; 
 import CompanyDetailPage from "./pages/CompanyDetials";
+import PostJobPage from "@/pages/job.pages/PostJobPage"; 
+import JobDetailsPage from "@/pages/job.pages/JobDetailsPage"; 
+
 
 const AppRoutes = () => {
   const [role, setRole] = useState();
@@ -277,6 +281,7 @@ const AppRoutes = () => {
       <Route path="/reports/:id" element={<ViewReport userRole={role} />} />
 
       <Route path="/view-report" element={<ViewReport />} />
+      <Route path="/network/company/:id" element={<CompanyDetailPage />} />
       
     </Routes>
   );
